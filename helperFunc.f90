d178634f6a59aa779a0127c87414561ea2a38bb6
abatista-planas <albgzz@mst.edu>
2023-08-30 11:53:06 -0500

    Addapting TotalEnergy_Calc  to FitConstants module and moving Read_Param function into module FitConstants

--- helperFunc.f90
+++ helperFunc.f90
@@ -149,58 +149,27 @@ SUBROUTINE Generate_Coordenates(coordenates,cal_coord,Ar,Br,C)
 End   SUBROUTINE Generate_Coordenates
 
 
+SUBROUTINE TotalEnergy_Calc (cal_coord,Ar,Br,C,ind,TotalEnergy,doTesting,testErr)
+ use FitConstants
+ implicit none
 
-SUBROUTINE TotalEnergy_Calc (cal_coord,Ar,Br,C,CoeffIndex,TotalEnergy,doTesting,testErr)
-
-    use FitConstants
-    implicit none
-    real*8, parameter ::  C1=627.5095d0,C2=0.529177249d0,Const=349.757d0
-
-
+ 
+    Integer, INTENT(IN):: ind ! index of the coefficents 
     real*8 , dimension(11) , INTENT(IN):: cal_coord
     real*8 , dimension(3), INTENT(IN):: Ar 
     real*8 , dimension(3), INTENT(IN):: Br
     real*8 , dimension(9), INTENT(IN):: C
+    real*8  , INTENT(INOut) ::TotalEnergy,testErr(52)
+    integer, INTENT(IN)::doTesting
 
-    real*8, INTENT(INOUT) ::TotalEnergy
-    integer,intent(in)::CoeffIndex,doTesting
-    real*8,intent(INOUT)::testErr(52)
-
-
-    real*8   ::Ene,EM,ED,EH,EI,T10,T20,T30,T40,cal_coord_temp(11)
+    real*8   ::Ene,EM,ED,EH,EI,cal_coord_temp(11)
     Integer :: n ;
-    real*8::Multipole_Energies(8),Ind_Energ(5),Hyp_Energ(2),Dispersion_Energies(3)
-
-    real*8 , dimension(64) :: A_Mult,B_Mult
-    real*8 , dimension(57) :: A_Pol,B_Pol
-    real*8 , dimension(40) :: A_HPol,B_HPol
-    real*8 , dimension(873):: Disp_AB
-
-    Integer, dimension (8):: M_Fit      
-    Integer, dimension (3):: D_Fit     
-    Integer, dimension (5):: I_Fit     
-    Integer, dimension (2):: H_Fit  
 
     real*8 :: Elect_energy(9),Dispe_energy(4),Induc_energy(6),Hyper_energy(3),term
 
     
 
     cal_coord_temp = cal_coord
-
-    M_Fit = Coeff(CoeffIndex)%M_Fit
-    I_Fit = Coeff(CoeffIndex)%I_Fit
-    D_Fit = Coeff(CoeffIndex)%D_Fit
-    H_Fit = Coeff(CoeffIndex)%H_Fit
-
-    A_Mult = Coeff(CoeffIndex)%A_Mult
-    B_Mult = Coeff(CoeffIndex)%A_Mult
-    A_Pol  = Coeff(CoeffIndex)%A_Pol
-    B_Pol  = Coeff(CoeffIndex)%B_Pol
-    A_HPol = Coeff(CoeffIndex)%A_HPol
-    B_HPol = Coeff(CoeffIndex)%B_HPol
-    Disp_AB= Coeff(CoeffIndex)%Disp
-
-
    
   
      Ene = 0.d0
@@ -215,31 +184,28 @@ SUBROUTINE TotalEnergy_Calc (cal_coord,Ar,Br,C,CoeffIndex,TotalEnergy,doTesting,
      Hyper_energy  = 0.d0
 
 
-
-
-
     do n = 1, 8
-        IF (M_Fit(n) > 0) THEN
+        IF ( Coeff(ind)%M_Fit(n) > 0) THEN
             if (n==1)Then
-                Call Approx_1_Sph2(cal_coord_temp,Ar,Br,C , A_Mult,B_Mult ,EM)
+                Call Approx_1_Sph2(cal_coord_temp,Ar,Br,C , Coeff(ind)%A_Mult,Coeff(ind)%B_Mult ,EM)
             elseif(n==2)Then
-                Call Approx_2_Sph2(cal_coord_temp,Ar,Br,C , A_Mult,B_Mult ,EM)
+                Call Approx_2_Sph2(cal_coord_temp,Ar,Br,C , Coeff(ind)%A_Mult,Coeff(ind)%B_Mult ,EM)
             elseif(n==3)Then
-                Call Approx_3_Sph2(cal_coord_temp,Ar,Br,C , A_Mult,B_Mult ,EM)
+                Call Approx_3_Sph2(cal_coord_temp,Ar,Br,C , Coeff(ind)%A_Mult,Coeff(ind)%B_Mult ,EM)
             elseif(n==4)Then
-                Call Approx_4_Sph2(cal_coord_temp,Ar,Br,C , A_Mult,B_Mult ,EM)    
+                Call Approx_4_Sph2(cal_coord_temp,Ar,Br,C , Coeff(ind)%A_Mult,Coeff(ind)%B_Mult ,EM)    
             elseif (n==5)Then
-                Call Approx_5_Sph2(cal_coord_temp,Ar,Br,C , A_Mult,B_Mult ,EM)
+                Call Approx_5_Sph2(cal_coord_temp,Ar,Br,C , Coeff(ind)%A_Mult,Coeff(ind)%B_Mult ,EM)
             elseif(n==6)Then
-                Call Approx_6_Sph2(cal_coord_temp,Ar,Br,C , A_Mult,B_Mult ,EM)
+                Call Approx_6_Sph2(cal_coord_temp,Ar,Br,C , Coeff(ind)%A_Mult,Coeff(ind)%B_Mult ,EM)
             elseif(n==7)Then
-                Call Approx_7_Sph2(cal_coord_temp,Ar,Br,C , A_Mult,B_Mult ,EM)
+                Call Approx_7_Sph2(cal_coord_temp,Ar,Br,C , Coeff(ind)%A_Mult,Coeff(ind)%B_Mult ,EM)
             elseif(n==8)Then
-                Call Approx_8_Sph2(cal_coord_temp,Ar,Br,C , A_Mult,B_Mult ,EM)         
+                Call Approx_8_Sph2(cal_coord_temp,Ar,Br,C , Coeff(ind)%A_Mult,Coeff(ind)%B_Mult ,EM)         
             endif
 
             term = (C1*C2**n)*EM
-            testErr(5 + n) = Const*term
+            testErr(5 + n) = C3*term
             Elect_energy(1+n) = term
             Elect_energy(1) = Elect_energy(1)+term 
             Ene = Ene+term
@@ -251,44 +217,51 @@ SUBROUTINE TotalEnergy_Calc (cal_coord,Ar,Br,C,CoeffIndex,TotalEnergy,doTesting,
 
 
      do n = 6,8
-        IF (D_Fit(n-5) > 0) THEN
+        IF (Coeff(ind)%D_Fit(n-5) > 0) THEN
             if(n==6)Then
-                Call Dispersion_6_Sph2(cal_coord_temp,Ar,Br,C, Disp_AB  ,ED)
+                Call Dispersion_6_Sph2(cal_coord_temp,Ar,Br,C, Coeff(ind)%Disp  ,ED)
             elseif(n==7)Then
-                Call Dispersion_7_Sph2(cal_coord_temp,Ar,Br,C, Disp_AB  ,ED)
+                Call Dispersion_7_Sph2(cal_coord_temp,Ar,Br,C, Coeff(ind)%Disp  ,ED)
             elseif(n==8)Then
-                Call Dispersion_8_Sph2(cal_coord_temp,Ar,Br,C, Disp_AB  ,ED)         
+                Call Dispersion_8_Sph2(cal_coord_temp,Ar,Br,C, Coeff(ind)%Disp  ,ED)         
             endif
 
             term = (C1*C2**n)*ED
-            testErr(20 + n - 5) = Const*term
+            testErr(20 + n - 5) = C3*term
             Dispe_energy(n-4) = term
             Dispe_energy(1) = Dispe_energy(1)+term
             Ene = Ene+term
+          
          END IF 
         
      end do
 
 
      do n = 4, 8
-        IF (I_Fit(n-3) > 0) THEN
+        IF (Coeff(ind)%I_Fit(n-3) > 0) THEN
             if(n==4)Then
-                Call Induction_4_Sph2(cal_coord_temp,Ar,Br,C , A_Mult,B_Mult ,A_Pol,B_Pol ,EI)    
+                Call Induction_4_Sph2(cal_coord_temp,Ar,Br,C , Coeff(ind)%A_Mult,Coeff(ind)%B_Mult ,&
+                Coeff(ind)%A_Pol,Coeff(ind)%B_Pol ,EI)    
             elseif (n==5)Then
-                Call Induction_5_Sph2(cal_coord_temp,Ar,Br,C , A_Mult,B_Mult ,A_Pol,B_Pol  ,EI)
+                Call Induction_5_Sph2(cal_coord_temp,Ar,Br,C , Coeff(ind)%A_Mult,Coeff(ind)%B_Mult ,&
+                Coeff(ind)%A_Pol,Coeff(ind)%B_Pol  ,EI)
             elseif(n==6)Then
-                Call Induction_6_Sph2(cal_coord_temp,Ar,Br,C , A_Mult,B_Mult ,A_Pol,B_Pol  ,EI)
+                Call Induction_6_Sph2(cal_coord_temp,Ar,Br,C , Coeff(ind)%A_Mult,Coeff(ind)%B_Mult ,&
+                Coeff(ind)%A_Pol,Coeff(ind)%B_Pol  ,EI)
             elseif(n==7)Then
-                Call Induction_7_Sph2(cal_coord_temp,Ar,Br,C , A_Mult,B_Mult ,A_Pol,B_Pol  ,EI)
+                Call Induction_7_Sph2(cal_coord_temp,Ar,Br,C , Coeff(ind)%A_Mult,Coeff(ind)%B_Mult ,&
+                Coeff(ind)%A_Pol,Coeff(ind)%B_Pol  ,EI)
             elseif(n==8)Then
-                Call Induction_8_Sph2(cal_coord_temp,Ar,Br,C , A_Mult,B_Mult ,A_Pol,B_Pol  ,EI)         
+                Call Induction_8_Sph2(cal_coord_temp,Ar,Br,C , Coeff(ind)%A_Mult,Coeff(ind)%B_Mult ,&
+                Coeff(ind)%A_Pol,Coeff(ind)%B_Pol  ,EI)         
             endif
 
             term = (C1*C2**n)*EI
-            testErr(30 + n-3) = Const*term
+            testErr(30 + n-3) = C3*term
             Induc_energy(n-2) = term
             Induc_energy(1) = Induc_energy(1)+term
             Ene = Ene+term
+            
          END IF 
         
      end do
@@ -296,231 +269,34 @@ SUBROUTINE TotalEnergy_Calc (cal_coord,Ar,Br,C,CoeffIndex,TotalEnergy,doTesting,
 
 
      do n = 6, 7
-        IF (H_Fit(n-5) > 0) THEN
+        IF (Coeff(ind)%H_Fit(n-5) > 0) THEN
             if(n==6)Then
-                Call HyperPolarizability_6_Sph2(cal_coord_temp,Ar,Br,C , A_Mult,B_Mult ,A_HPol,B_HPol,EH)
+                Call HyperPolarizability_6_Sph2(cal_coord_temp,Ar,Br,C , Coeff(ind)%A_Mult,&
+                                    Coeff(ind)%B_Mult ,Coeff(ind)%A_HPol,Coeff(ind)%B_HPol,EH)
             elseif(n==7)Then
-                Call HyperPolarizability_7_Sph2(cal_coord_temp,Ar,Br,C , A_Mult,B_Mult ,A_HPol,B_HPol,EH)   
+                Call HyperPolarizability_7_Sph2(cal_coord_temp,Ar,Br,C , Coeff(ind)%A_Mult,&
+                                    Coeff(ind)%B_Mult ,Coeff(ind)%A_HPol,Coeff(ind)%B_HPol,EH)   
             endif
             term = (C1*C2**n)*EH
-            testErr(42 + n-5) = Const*term
+            testErr(42 + n-5) = C3*term
             Hyper_energy(n-4) = term
             Hyper_energy(1) = Hyper_energy(1)+term
             Ene = Ene+term
+        
          END IF 
         
      end do
 
 
 
-    TotalEnergy  = Const*Ene
+    TotalEnergy  = C3*Ene
     if (doTesting>0)Then
         testErr(1) = TotalEnergy
-        testErr(2) = Const*Elect_energy(1)
-        testErr(3) = Const*Dispe_energy(1)
-        testErr(4) = Const*Induc_energy(1)
-        testErr(5) = Const*Hyper_energy(1)
+        testErr(2) = C3*Elect_energy(1)
+        testErr(3) = C3*Dispe_energy(1)
+        testErr(4) = C3*Induc_energy(1)
+        testErr(5) = C3*Hyper_energy(1)
     end if 
 
 
 end SUBROUTINE TotalEnergy_Calc
-
-
-
-! SUBROUTINE Prep_Param(Coeff_Address, coeff_arr,M_Fit ,D_Fit,I_Fit,H_Fit,Zero)
-!     IMPLICIT NONE
-
-!     !Multipoles !
-    
-!     real*8 , dimension(1)   :: qA,qB !q
-!     real*8 , dimension(3)   :: mA,mB !m
-!     real*8 , dimension(5)   :: QdA,QdB !Qd 
-!     real*8 , dimension(7)   :: OA,OB !O 
-!     real*8 , dimension(9)   :: PhiA,PhiB !Phi 
-!     real*8 , dimension(11)   :: M5A,M5B !M5
-!     real*8 , dimension(13)   :: M6A,M6B !M6 
-!     real*8 , dimension(15)   :: M7A,M7B !M7
-
-!     real*8 , dimension(64)   :: A_Mult,B_Mult 
-
-!     !Polarizability!
-
-!     real*8 , dimension(6)   :: mmA,mmB 
-!     real*8 , dimension(15)  :: mQdA,mQdB
-!     real*8 , dimension(15)  :: QdQdA,QdQdB
-!     real*8 , dimension(21)  :: mOA,mOB
-
-!     real*8 , dimension(57)   :: A_Pol,B_Pol 
-
-!     !Hyperpolarizability!
-
-!     real*8 , dimension(10)   :: mmmA,mmmB 
-!     real*8 , dimension(30)  :: mmQdA,mmQdB
-
-!     real*8 , dimension(40)   :: A_HPol,B_HPol 
-
-!     !Dispersion!
-
-!     real*8 , dimension(36)   :: mm_mm 
-!     real*8 , dimension(90)  ::  mm_mQd,mQd_mm,mm_QdQd,QdQd_mm
-!     real*8 , dimension(126)   :: mO_mm,mm_mO 
-!     real*8 , dimension(225)   :: mQd_mQd 
-
-!     real*8 , dimension(873)   :: Disp
-
-
-
-!     real*8 , dimension(1195), INTENT(INOUT) :: coeff_arr
-
-!     Character(len = *), INTENT(IN)   ::  Coeff_Address
-
-!     Integer, dimension (8),  INTENT(INOUT) :: M_Fit      
-!     Integer, dimension (3),  INTENT(INOUT) :: D_Fit     
-!     Integer, dimension (5),  INTENT(INOUT) :: I_Fit     
-!     Integer, dimension (2),  INTENT(INOUT) :: H_Fit     
-
-!     Real*8 , INTENT(out) :: Zero                         
-
-
-!     Character(len = 20) :: row
-!     ! Integer , dimension(7) :: DataColumn ! R , Cos_b1 , Cos_b2 , alpha ,  Cos_c1 , Cos_c2 , Energy    
-
-!     !write(*,*) 'Reading file'
-
-!     Open( 10, file = Coeff_Address )
-
-!     Read( 10, *) row
-!     Read( 10, *) row
-!     Read( 10, *) row
-!     Read( 10, *) row
-!     Read( 10, *) row
-!     Read( 10, *) row
-!     Read( 10, *) row
-
-!     read(10, *)  M_Fit
-!     read(10, *)  D_Fit
-!     read(10, *)  I_Fit
-!     read(10, *)  H_Fit
-
-!     Read( 10, *) row
-!     read(10, *) Zero
-
-!     Read( 10, *) row
-!     read(10, *) qA
-!     read(10, *) mA
-!     read(10, *) QdA
-!     read(10, *) OA
-!     read(10, *) PhiA
-!     read(10, *) M5A
-!     read(10, *) M6A
-!     read(10, *) M7A
-
-!     Read( 10, *) row
-!     read(10, *) qB
-!     read(10, *) mB
-!     read(10, *) QdB
-!     read(10, *) OB
-!     read(10, *) PhiB
-!     read(10, *) M5B
-!     read(10, *) M6B
-!     read(10, *) M7B
-
-!     Read( 10, *) row
-!     read(10, *) mmA
-!     read(10, *) mQdA
-!     read(10, *) QdQdA
-!     read(10, *) mOA
-
-!     Read( 10, *) row
-!     read(10, *) mmB
-!     read(10, *) mQdB
-!     read(10, *) QdQdB
-!     read(10, *) mOB
-
-!     Read( 10, *) row
-!     read(10, *) mmmA
-!     read(10, *) mmQdA
-
-    
-!     Read( 10, *) row
-!     read(10, *) mmmB
-!     read(10, *) mmQdB
-
-!     Read( 10, *) row
-!     read(10, *) mm_mm
-!     read(10, *) mQd_mm
-!     read(10, *) mm_mQd
-!     read(10, *) mO_mm
-!     read(10, *) mm_mO
-!     read(10, *) QdQd_mm
-!     read(10, *) mm_QdQd
-!     read(10, *) mQd_mQd
-
-
-!     close(10) 
-
-
-
-!     A_Mult(1:1) = qA
-!     A_Mult(2:4) = mA
-!     A_Mult(5:9) = QdA
-!     A_Mult(10:16) = OA
-!     A_Mult(17:25) = PhiA
-!     A_Mult(26:36) = M5A
-!     A_Mult(37:49) = M6A
-!     A_Mult(50:64) = M7A
-
-!     B_Mult(1:1) = qB
-!     B_Mult(2:4) = mB
-!     B_Mult(5:9) = QdB
-!     B_Mult(10:16) = OB
-!     B_Mult(17:25) = PhiB
-!     B_Mult(26:36) = M5B
-!     B_Mult(37:49) = M6B
-!     B_Mult(50:64) = M7B
-
-
-!     A_Pol(1:6) = mmA
-!     A_Pol(7:21) = mQdA
-!     A_Pol(22:36) = QdQdA
-!     A_Pol(37:57) = mOA
-
-    
-!     B_Pol(1:6) = mmB
-!     B_Pol(7:21) = mQdB
-!     B_Pol(22:36) = QdQdB
-!     B_Pol(37:57) = mOB
-
-!     A_HPol(1:10) = mmmA
-!     A_HPol(11:40) = mmQdA
-
-!     B_HPol(1:10) = mmmB
-!     B_HPol(11:40) = mmQdB
-
-!     Disp(1:36) = mm_mm
-!     Disp(37:126) = mQd_mm
-!     Disp(127:216) = mm_mQd
-!     Disp(217:342) = mO_mm
-!     Disp(343:468) = mm_mO
-!     Disp(469:558) = QdQd_mm
-!     Disp(559:648) = mm_QdQd
-!     Disp(649:873) = mQd_mQd
-
-
-!     coeff_arr(1:64)   = A_Mult
-!     coeff_arr(65:128) = B_Mult
-
-!     coeff_arr(129:185)   = A_Pol
-!     coeff_arr(186:242)   = B_Pol
-
-!     coeff_arr(243:282)   = A_HPol
-!     coeff_arr(283:322)   = B_HPol
-
-!     coeff_arr(323:1195)   = Disp
-
-
-!     RETURN
-! END SUBROUTINE Prep_Param
-
-
-

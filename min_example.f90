8a3414ea54ecb7ad4f23e1f76d48338c2e103911
abatista-planas <133795152+abatista-planas@users.noreply.github.com>
2024-09-18 19:50:11 -0500

    minimalistic version of min_example

--- min_example.f90
+++ min_example.f90
@@ -4,10 +4,6 @@ PROGRAM min_example
  real*8 :: E1
  INTEGER :: XDIM=4
  real*8 :: X1(4)
- real*8::Energy
- real*8, allocatable:: arrp(:),arrh(:),arrd(:)
- character(len=5),allocatable::arr(:)
- integer ::i,j,k,l,index1,index2,counter
 
  X1(1) = 10.271963668339600d0 !R
  X1(2) = 30d0  !b1

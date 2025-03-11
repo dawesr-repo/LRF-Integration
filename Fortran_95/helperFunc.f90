ba85c00bc0ea352c96e539dc6af69a3c99802321
abatista-planas <133795152+abatista-planas@users.noreply.github.com>
2025-03-11 13:35:18 -0500

    pass coordinate format by as a parameter in "evaluate_LR"

--- Fortran_95/helperFunc.f90
+++ Fortran_95/helperFunc.f90
@@ -1,75 +1,75 @@
-SUBROUTINE Coordinate_Transformation(coordenates,coord_format,new_coordinates)
+SUBROUTINE Coordinate_Transformation(coordinates,coord_format,new_coordinates)
     IMPLICIT NONE
-    real*8 ,dimension(6), INTENT(IN):: coordenates
-    
-    Character(len = 20), INTENT(IN) :: coord_format
-    real*8 , dimension(6), INTENT(INOUT) :: new_coordinates
-    
-    new_coordinates=coordenates;
+    real*8 ,dimension(6), INTENT(IN):: coordinates
 
-    
+    Character(*), INTENT(IN) :: coord_format
+    real*8 , dimension(6), INTENT(INOUT) :: new_coordinates
+
+    new_coordinates=coordinates;
+
+
     if (coord_format == "Euler_ZYZ") then
-        new_coordinates(5) = coordenates(5) - 90
-        new_coordinates(6) = coordenates(6) - 90
-    end if 
+        new_coordinates(5) = coordinates(5) - 90
+        new_coordinates(6) = coordinates(6) - 90
+    end if
     if (coord_format == "Spherical") then
-        new_coordinates(5) = 90 - coordenates(5) 
-        new_coordinates(6) = 90 -coordenates(6) 
-    end if 
-    
+        new_coordinates(5) = 90 - coordinates(5)
+        new_coordinates(6) = 90 -coordinates(6)
+    end if
+
 End   SUBROUTINE Coordinate_Transformation
 
-    !********************************************************
+!********************************************************
 SUBROUTINE General_Coordinates_Format(Dim, old_coordinates, general_coodinates)
-            IMPLICIT NONE
-    
-            INTEGER,INTENT(IN) ::  Dim
-            real*8 , dimension(6), INTENT(INOUT) ::general_coodinates
-            real*8 , dimension (Dim),INTENT(IN) :: old_coordinates
-       
+    IMPLICIT NONE
 
-            if (Dim==2) then
-                    general_coodinates(1) = old_coordinates(1)  !R
-                    general_coodinates(2) = old_coordinates(2)  !b1
-                    general_coodinates(3) = 0d0  !b2
-                    general_coodinates(4) = 0d0  !phi
-                    general_coodinates(5) = 0d0  !c1
-                    general_coodinates(6) = 0d0  !c2
+    INTEGER,INTENT(IN) ::  Dim
+    real*8 , dimension(6), INTENT(INOUT) ::general_coodinates
+    real*8 , dimension (Dim),INTENT(IN) :: old_coordinates
 
-                elseif (Dim==3) then
-                    general_coodinates(1) = old_coordinates(1)  !R
-                    general_coodinates(2) = old_coordinates(2)  !b1
-                    general_coodinates(3) = 0d0  !b2
-                    general_coodinates(4) = 0d0  !phi
-                    general_coodinates(5) = old_coordinates(3)  !c1
-                    general_coodinates(6) = 0d0  !c2
 
-                elseif (Dim==4) then   
-                    general_coodinates(1) = old_coordinates(1)  !R
-                    general_coodinates(2) = old_coordinates(2)  !b1
-                    general_coodinates(3) = old_coordinates(3)  !b2
-                    general_coodinates(4) = old_coordinates(4)  !phi
-                    general_coodinates(5) = 0d0  !c1
-                    general_coodinates(6) = 0d0  !c2 
+    if (Dim==2) then
+        general_coodinates(1) = old_coordinates(1)  !R
+        general_coodinates(2) = old_coordinates(2)  !b1
+        general_coodinates(3) = 0d0  !b2
+        general_coodinates(4) = 0d0  !phi
+        general_coodinates(5) = 0d0  !c1
+        general_coodinates(6) = 0d0  !c2
 
-                elseif (Dim==5) then   
-                        general_coodinates(1) = old_coordinates(1)  !R
-                        general_coodinates(2) = old_coordinates(2)  !b1
-                        general_coodinates(3) = old_coordinates(3)  !b2
-                        general_coodinates(4) = old_coordinates(4)  !phi
-                        general_coodinates(5) = old_coordinates(5)  !c1
-                        general_coodinates(6) = 0d0  !c2     
-                elseif (Dim==6) then   
-                        general_coodinates(1) = old_coordinates(1)  !R
-                        general_coodinates(2) = old_coordinates(2)  !b1
-                        general_coodinates(3) = old_coordinates(3)  !b2
-                        general_coodinates(4) = old_coordinates(4)  !phi
-                        general_coodinates(5) = old_coordinates(5)  !c1
-                        general_coodinates(6) = old_coordinates(6)  !c2       
+    elseif (Dim==3) then
+        general_coodinates(1) = old_coordinates(1)  !R
+        general_coodinates(2) = old_coordinates(2)  !b1
+        general_coodinates(3) = 0d0  !b2
+        general_coodinates(4) = 0d0  !phi
+        general_coodinates(5) = old_coordinates(3)  !c1
+        general_coodinates(6) = 0d0  !c2
 
-            end if
-  
-            RETURN
+    elseif (Dim==4) then
+        general_coodinates(1) = old_coordinates(1)  !R
+        general_coodinates(2) = old_coordinates(2)  !b1
+        general_coodinates(3) = old_coordinates(3)  !b2
+        general_coodinates(4) = old_coordinates(4)  !phi
+        general_coodinates(5) = 0d0  !c1
+        general_coodinates(6) = 0d0  !c2
+
+    elseif (Dim==5) then
+        general_coodinates(1) = old_coordinates(1)  !R
+        general_coodinates(2) = old_coordinates(2)  !b1
+        general_coodinates(3) = old_coordinates(3)  !b2
+        general_coodinates(4) = old_coordinates(4)  !phi
+        general_coodinates(5) = old_coordinates(5)  !c1
+        general_coodinates(6) = 0d0  !c2
+    elseif (Dim==6) then
+        general_coodinates(1) = old_coordinates(1)  !R
+        general_coodinates(2) = old_coordinates(2)  !b1
+        general_coodinates(3) = old_coordinates(3)  !b2
+        general_coodinates(4) = old_coordinates(4)  !phi
+        general_coodinates(5) = old_coordinates(5)  !c1
+        general_coodinates(6) = old_coordinates(6)  !c2
+
+    end if
+
+    RETURN
 END SUBROUTINE General_Coordinates_Format
 
 
@@ -78,10 +78,10 @@ END SUBROUTINE General_Coordinates_Format
 
 SUBROUTINE TotalEnergy_Calc (ind,TotalEnergy)
 
- implicit none
+    implicit none
 
- 
-    Integer, INTENT(IN):: ind ! index of the coefficents 
+
+    Integer, INTENT(IN):: ind ! index of the coefficents
     real*8  , INTENT(INOut) ::TotalEnergy
 
     real*8   ::EM,ED,EI,term
@@ -93,79 +93,55 @@ SUBROUTINE TotalEnergy_Calc (ind,TotalEnergy)
     !print*, "Electrostatic Energy: ", EM, " Induction Energy: ", EI, " Dispersion Energy: ", ED
 
 
-    TotalEnergy = EM + ED + EI  
+    TotalEnergy = EM + ED + EI
 
 
 end SUBROUTINE TotalEnergy_Calc
 
 
 
-! Arg 1 [coordenates] : a coordenate vector [ R , b1, b2, phi] *the angles should be in degrees  
-! Arg 2 [Coeff_Address] address of the file which contains the longe range expansion coefficients   
-! Arg 3 [TotalEnergy]   Total Energy calculated 
+! Arg 1 [coordinates] : a coordinate vector [ R , b1, b2, phi] *the angles should be in degrees
+! Arg 2 [Coeff_Address] address of the file which contains the longe range expansion coefficients
+! Arg 3 [TotalEnergy]   Total Energy calculated
 
 
 ! version 4.0
 
 
+SUBROUTINE Evaluate_LRF(TotalEnergy,coordinates,coord_format,XDIM,filename)
 
-SUBROUTINE Long_Range_Potential(coordenates,TotalEnergy,filename)
+    use FitConstants, only: Coeff, max_T, Get_Coeff_Index
+    use Geometry_Constant_v2, only: init_Tensors_v2
 
- use FitConstants, only: Coeff, max_T, Get_Coeff_Index
- use Geometry_Constant_v2, only: init_Tensors_v2
- IMPLICIT NONE
+    IMPLICIT NONE
 
- real*8, INTENT(INOUT)  ::  TotalEnergy
- real*8 ,dimension(6), INTENT(IN)  :: coordenates ! the angles are in degree
- character(len = *), INTENT(IN) :: filename
- real*8::testErr(52),T1,T2,r1
+    real*8, INTENT(OUT) :: TotalEnergy
+    INTEGER, INTENT(IN) :: XDIM
+    real*8 ,dimension(:), INTENT(IN):: coordinates(XDIM)
+    Character(*), INTENT(IN) :: coord_format
+    Character(*), INTENT(IN) ::  filename
+    real*8 ,dimension(6):: GeneralCoordenates,GeneralCoordenates1
+    INTEGER :: i,CoeffIndex
+    real*8 :: x1
 
- integer::CoeffIndex
- Character(len = 1), dimension(3) :: cpn = ["0","c","s"]
+    call Get_Coeff_Index(filename,CoeffIndex)
 
+    x1 = 0d0
+    do i=1,XDIM
+        x1=x1+dabs(coordinates(i))
+    enddo
 
- if (coordenates(1)==0d0 .and. coordenates(2)==0d0 .and. coordenates(3)==0d0 .and. coordenates(4)==0d0 &
-     .and. coordenates(5)==0d0 .and. coordenates(6)==0d0) THEN
-    TotalEnergy = Coeff(CoeffIndex)%Zero
- else
-    
-    call Get_Coeff_Index(filename,CoeffIndex) ! Initializing coefficients Fit for the file named as "filename"
-    
-    call init_Tensors_v2(max_T,coordenates) ! Initializing in zero the new vectors v2
+    if (x1 <= 1d-10) then
+        TotalEnergy = Coeff(CoeffIndex)%Zero
+        return
+    endif
 
+    Call General_Coordinates_Format(XDIM, coordinates, GeneralCoordenates)
+    Call Coordinate_Transformation(GeneralCoordenates,coord_format,GeneralCoordenates1)
+
+    Call init_Tensors_v2(max_T,GeneralCoordenates1) ! Initializing in zero the new vectors v2
     Call TotalEnergy_Calc (CoeffIndex,TotalEnergy)
-    !print*, "Total Energy: ", TotalEnergy
-  end if     
 
-END SUBROUTINE Long_Range_Potential
-
-
-SUBROUTINE evaluateLR(coordinates,XDIM,E1,filename)
-  IMPLICIT NONE
-
-  real*8, INTENT(OUT) :: E1
-  INTEGER, INTENT(IN) :: XDIM
-  real*8 ,dimension(:), INTENT(IN):: coordinates(XDIM)
-  Character(len = 20) :: coord_format = "Euler_ZYZ" !for Xdim =3, use coord_format ="Spherical" for Autosurf input 
-  real*8 ,dimension(6):: GeneralCoordenates,GeneralCoordenates1
-  INTEGER :: i
-  real*8 :: x1
-  Character(*), INTENT(IN) ::  filename
-      
-  x1=0d0
-  do i=1,XDIM
-    x1=x1+dabs(coordinates(i))
-  enddo
-
-  if (x1 <= 1d-10) then
-    GeneralCoordenates=0d0
-    CALL Long_Range_Potential(GeneralCoordenates,E1,filename)
     return
-  endif
 
-  Call General_Coordinates_Format(XDIM, coordinates, GeneralCoordenates)
-  Call Coordinate_Transformation(GeneralCoordenates,coord_format,GeneralCoordenates1)
-  CALL Long_Range_Potential(GeneralCoordenates1,E1,filename)
-  return
-
-END SUBROUTINE evaluateLR
+END SUBROUTINE Evaluate_LRF
--- Fortran_95/min_example.f90
+++ Fortran_95/min_example.f90
@@ -1,18 +1,23 @@
 PROGRAM min_example
 
  IMPLICIT NONE
- real*8 :: E1
- INTEGER :: XDIM=4
- real*8 :: X1(4)
 
- X1(1) = 10.271963668339600d0 !R
- X1(2) = 30d0  !b1
- X1(3) = 20d0  !b2
- X1(4) = 120d0  !Phi
+ INTEGER :: xdim=4
+ real*8 :: coordinates(4)
+ real*8 :: Energy
+ coordinates(1) = 10.271963668339600d0 !R
+ coordinates(2) = 30d0  !b1
+ coordinates(3) = 20d0  !b2
+ coordinates(4) = 120d0  !Phi
 
 
- call evaluateLR(X1,XDIM,E1,'./files/test/coefficients/C1(1)_C1(1)_Coeff.txt')
-  
+ call Evaluate_LRF(   Energy,&
+         coordinates,&
+         "Euler_ZYZ",&
+         xdim,&
+         '../testing_datafiles/coefficients/C1(1)_C1(1)_Coeff.txt')
+
+
 END PROGRAM min_example
 
 
